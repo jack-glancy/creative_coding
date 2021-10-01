44c0cbfd0e00fc740ac31b4d9b135d4c7da66d56
Jack Glancy <83410114+jack-glancy@users.noreply.github.com>
2021-10-01 09:46:51 +0100

    Create diet_options.py

---
+++ diet_options.py
@@ -0,0 +1,10 @@
+class diet_options:
+    dict = {
+        'All meat-eaters' : (5.93, 5.71, 5.76),
+        'High meat-eaters (≥100 g/day)' : (7.26, 7.17, 7.19),
+        'Medium meat-eaters (50–99 g/day)' : (5.66, 5.62, 5.63),
+        'Low meat-eaters (<50 g/day)' : (4.67, 4.67, 4.67),
+        'Fish-eaters' : (3.94, 3.90, 3.91),
+        'Vegetarians' : (3.85, 3.80, 3.81),
+        'Vegans' : (2.94, 2.87, 2.89)
+    }
